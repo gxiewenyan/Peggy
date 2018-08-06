f55ac10852f8fae910c5ff16557ee6e0f493d927
gxiewenyan <gxiewenyan@gmail.com>
2018-08-06 17:39:24 +0800

    init

---
+++ SQLScript/pg_offices.sql
@@ -0,0 +1,5 @@
+/*
+数据初始化
+*/
+
+insert  into `pg_offices`(`id`,`name`,`code`,`parent_id`,`parent_name`) values (1,'技术服务','00003',NULL,NULL),(2,'管理部','00001',NULL,NULL),(3,'检测综合技术室','00002',NULL,NULL),(4,'质管部','00004',NULL,NULL),(5,'科研中心','00005',NULL,NULL),(6,'信息中心','00006',NULL,NULL),(7,'业务受理室','00007',NULL,NULL),(8,'快检室','00008',NULL,NULL),(9,'新项目研发室','00009',NULL,NULL),(10,'检测办','00010',NULL,NULL),(11,'报告室（综合业务）','00011',NULL,NULL),(12,'样品室','00012',NULL,NULL),(13,'抽样室','00013',NULL,NULL),(14,'理化室','00014',NULL,NULL),(15,'气相室','00015',NULL,NULL),(16,'微生物室','00016',NULL,NULL),(17,'液相室','00017',NULL,NULL),(18,'光谱室','00018',NULL,NULL),(19,'氨基酸室','00019',NULL,NULL),(20,'分子室','00020',NULL,NULL);
