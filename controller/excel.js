e7c61a5837cefc8974ff9461dbce4dc19bd441b1
gxiewenyan <gxiewenyan@gmail.com>
2018-08-06 17:54:33 +0800

    增加excel to html测试代码

--- controller/excel.js
+++ controller/excel.js
@@ -1,18 +1,9 @@
 const XLSX = require('xlsx');
 const path = require('path');
 const fs = require('fs');
-const mysql = require('mysql2/promise');
-const constants = require('../constants');
+// const mysql = require('mysql2/promise');
+// const constants = require('../constants');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '123456',
-    database: 'peggy',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
 
 module.exports = {
     parse: async (ctx, next) => {
@@ -45,20 +36,37 @@ module.exports = {
     },
     uploadHandler: async (ctx, next) => {
         console.log('handle upload...');
-        console.log(ctx.request.files.file);
-        // console.log(ctx.request.body.files);
 
         const file = ctx.request.files.file;
         const reader = fs.createReadStream(file.path);	// 创建可读流
         const upStream = fs.createWriteStream('upload/' + file.name);		// 创建可写流
-        reader.pipe(upStream).on('finish', () => {
-            const workbook = XLSX.readFile(path.dirname(require.main.filename) + '/upload/' + file.name, {});
-            const sheetNames = workbook.SheetNames; // 返回 ['201801', '201802', ...]
+        reader.pipe(upStream);	// 可读流通过管道写入可写流
 
-        });	// 可读流通过管道写入可写流
+        // await ctx.render('upload_result', {result: true});
+        await module.exports.uploadResultHandler(ctx, next, file.name);
+    },
+    uploadResultHandler: async (ctx, next, fileName) => {
+        const workbook = XLSX.readFile('./upload/' + fileName, {});
 
+        // 获取 Excel 中所有表名
+        const sheetNames = workbook.SheetNames; // 返回 ['sheet1', 'sheet2'];
+        console.log('sheetNames =====> ' + sheetNames);
 
-        await ctx.render('upload_result', {result: true});
+        let sheetHtml = XLSX.utils.sheet_to_html(workbook.Sheets[sheetNames[0]]);
+        console.log(sheetHtml);
+
+        let contentData = [];
+        let reg = /<table>[\S|\s]*<\/table>/ig;
+
+        for(let i = 0, length = sheetNames.length; i < length; i++){
+            let sheetContentHtml = XLSX.utils.sheet_to_html(workbook.Sheets[sheetNames[i]]).match(reg);
+            contentData.push({
+                sheetName: sheetNames[i],
+                sheetContent: sheetContentHtml
+            });
+        }
+
+        await ctx.render('upload_result', {data: contentData});
     },
     save: async (ctx, next) => {
         // 检查记录是否已存在
