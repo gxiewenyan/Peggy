8e3de644fc2a10d1b3bc804e66dc4898de7f0ebc
wb_2506798638 <2506798638@qq.com>
2018-08-08 22:45:47 +0800

    导入实验

--- controller/excel.js
+++ controller/excel.js
@@ -7,29 +7,112 @@ const fs = require('fs');
 
 module.exports = {
     parse: async (ctx, next) => {
-        const workbook = XLSX.readFile('./upload/2017检测小组费用汇总.xlsx', {});
+        const workbook = XLSX.readFile('./upload/import.xlsx', {});
         // const workbook = XLSX.readFile('./upload/test.xlsx', {});
 
         // 获取 Excel 中所有表名
         const sheetNames = workbook.SheetNames; // 返回 ['sheet1', 'sheet2'];
 
-        let sheetHtml = XLSX.utils.sheet_to_html(workbook.Sheets[sheetNames[0]]);
-        console.log(sheetHtml);
+        let splitedSheetName = sheetNames[0].split('.');
+        let year = splitedSheetName[0],
+            month = splitedSheetName[1];
 
-        let contentData = [];
-        let reg = /<table>[\S|\s]*<\/table>/ig;
+        let officesNames = ['技术服务','管理部','检测综合技术室','质管部','科研中心','信息中心','业务受理室','快检室','新项目研发室','检测办','报告室（综合业务）','样品室','抽样室','理化室','气相室','微生物室','液相室','光谱室','氨基酸室','分子室'];
+        let officeIdMapping = [{
+            "name": "技术服务",
+            "id": 1
+        }, {
+            "name": "管理部",
+            "id": 2
+        }, {
+            "name": "检测综合技术室",
+            "id": 3
+        }, {
+            "name": "质管部",
+            "id": 4
+        }, {
+            "name": "科研中心",
+            "id": 5
+        }, {
+            "name": "信息中心",
+            "id": 6
+        }, {
+            "name": "业务受理室",
+            "id": 7
+        }, {
+            "name": "快检室",
+            "id": 8
+        }, {
+            "name": "新项目研发室",
+            "id": 9
+        }, {
+            "name": "检测办",
+            "id": 10
+        }, {
+            "name": "报告室（综合业务）",
+            "id": 11
+        }, {
+            "name": "样品室",
+            "id": 12
+        }, {
+            "name": "抽样室",
+            "id": 13
+        }, {
+            "name": "理化室",
+            "id": 14
+        }, {
+            "name": "气相室",
+            "id": 15
+        }, {
+            "name": "微生物室",
+            "id": 16
+        }, {
+            "name": "液相室",
+            "id": 17
+        }, {
+            "name": "光谱室",
+            "id": 18
+        }, {
+            "name": "氨基酸室",
+            "id": 19
+        }, {
+            "name": "分子室",
+            "id": 20
+        }];
 
-        for(let i = 0, length = sheetNames.length; i < length; i++){
-            let sheetContentHtml = XLSX.utils.sheet_to_html(workbook.Sheets[sheetNames[i]]).match(reg);
-            contentData.push({
-                sheetName: sheetNames[i],
-                sheetContent: sheetContentHtml
-            });
-        }
+        let col = 'B';
+        let address_of_cell = col + '1';
 
-        console.log('--0--' + contentData.length);
+        /* Get worksheet */
+        let worksheet = workbook.Sheets[sheetNames[0]];
 
-        await ctx.render('upload_result', {data: contentData});
+        /* Find desired cell */
+        let officeCell = worksheet[address_of_cell];
+
+        /* Get the value */
+        let officeCellValue = officeCell.v;
+
+        let officeId = officeIdMapping[officesNames.indexOf(officeCellValue)].id;
+
+        let labourCost = worksheet[col + '2'].v,
+            adminCost = worksheet[col + '14'].v,
+            depreciationCost = worksheet[col + '20'].v,
+            variableCost = worksheet[col + '21'].v;
+
+        let pgCost = {
+            office_id: officeId,
+            total_cost: parseFloat(labourCost) + parseFloat(adminCost) + parseFloat(depreciationCost) + parseFloat(variableCost),
+            labour_cost: labourCost,
+            administrative_cost: adminCost,
+            depreciation_cost: depreciationCost,
+            variable_cost: variableCost,
+            year: year,
+            month: month
+        };
+
+        console.log(pgCost);
+
+        ctx.body = 'ok';
     },
     upload: async (ctx, next) => {
         await ctx.render('upload');
