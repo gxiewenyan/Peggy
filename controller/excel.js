988f3b71cbe20e722933b8c90012f6291ec53de8
gxiewenyan <gxiewenyan@gmail.com>
2018-08-09 17:43:20 +0800

    导入数据

--- controller/excel.js
+++ controller/excel.js
@@ -3,19 +3,16 @@ const path = require('path');
 const fs = require('fs');
 // const mysql = require('mysql2/promise');
 // const constants = require('../constants');
+const costService = require('../service/cost');
 
 
 module.exports = {
     parse: async (ctx, next) => {
-        const workbook = XLSX.readFile('./upload/import.xlsx', {});
+        const workbook = XLSX.readFile('./upload/2017import.xlsx', {});
         // const workbook = XLSX.readFile('./upload/test.xlsx', {});
 
         // 获取 Excel 中所有表名
         const sheetNames = workbook.SheetNames; // 返回 ['sheet1', 'sheet2'];
-
-        let splitedSheetName = sheetNames[0].split('.');
-        let year = splitedSheetName[0],
-            month = splitedSheetName[1];
 
         let officesNames = ['技术服务','管理部','检测综合技术室','质管部','科研中心','信息中心','业务受理室','快检室','新项目研发室','检测办','报告室（综合业务）','样品室','抽样室','理化室','气相室','微生物室','液相室','光谱室','氨基酸室','分子室'];
         let officeIdMapping = [{
@@ -80,37 +77,115 @@ module.exports = {
             "id": 20
         }];
 
-        let col = 'B';
-        let address_of_cell = col + '1';
+        let cols = ['B','C','D','E','F','G','H','I','J','K','L',"M",'N','O','P','Q','R','S','T','U'];
 
-        /* Get worksheet */
-        let worksheet = workbook.Sheets[sheetNames[0]];
+        sheetNames.forEach((sheetName) => {
+            let splitedSheetName = sheetName.split('.');
+            let year = splitedSheetName[0],
+                month = splitedSheetName[1];
 
-        /* Find desired cell */
-        let officeCell = worksheet[address_of_cell];
+            cols.forEach(async (col) => {
+                let address_of_cell = col + '1';
 
-        /* Get the value */
-        let officeCellValue = officeCell.v;
+                /* Get worksheet */
+                let worksheet = workbook.Sheets[sheetName];
 
-        let officeId = officeIdMapping[officesNames.indexOf(officeCellValue)].id;
+                /* Find desired cell */
+                let officeCell = worksheet[address_of_cell];
 
-        let labourCost = worksheet[col + '2'].v,
-            adminCost = worksheet[col + '14'].v,
-            depreciationCost = worksheet[col + '20'].v,
-            variableCost = worksheet[col + '21'].v;
+                /* Get the value */
+                let officeCellValue = officeCell.v;
 
-        let pgCost = {
-            office_id: officeId,
-            total_cost: parseFloat(labourCost) + parseFloat(adminCost) + parseFloat(depreciationCost) + parseFloat(variableCost),
-            labour_cost: labourCost,
-            administrative_cost: adminCost,
-            depreciation_cost: depreciationCost,
-            variable_cost: variableCost,
-            year: year,
-            month: month
-        };
+                let officeId = officeIdMapping[officesNames.indexOf(officeCellValue)].id;
 
-        console.log(pgCost);
+                let labourCost = worksheet[col + '2'].v,
+                    adminCost = worksheet[col + '14'].v,
+                    depreciationCost = worksheet[col + '20'].v,
+                    variableCost = worksheet[col + '21'].v;
+
+                let pgCost = {
+                    office_id: officeId,
+                    total_cost: parseFloat(labourCost) + parseFloat(adminCost) + parseFloat(depreciationCost) + parseFloat(variableCost),
+                    labour_cost: labourCost,
+                    administrative_cost: adminCost,
+                    depreciation_cost: depreciationCost,
+                    variable_cost: variableCost,
+                    year: year,
+                    month: month
+                };
+
+                let salary = worksheet[col + '3'].v,
+                    socialSecurity = worksheet[col + '4'].v,
+                    commericalInsurance = worksheet[col + '5'].v,
+                    allowanace = worksheet[col + '6'].v,
+                    traning = worksheet[col + '7'].v,
+                    tradeUnion = worksheet[col + '8'].v,
+                    laborProtection = worksheet[col + '9'].v,
+                    housing = worksheet[col + '10'].v,
+                    techAward = worksheet[col + '11'].v,
+                    nonMonetary = worksheet[col + '12'].v,
+                    labourOther = worksheet[col + '13'].v;
+
+                let pgLabourCostDetails = {
+                    salary: salary,
+                    social_security: socialSecurity,
+                    commercial_insurance: commericalInsurance,
+                    allowance: allowanace,
+                    training: traning,
+                    trade_union: tradeUnion,
+                    labor_protection: laborProtection,
+                    housing: housing,
+                    tech_award: techAward,
+                    non_monetary: nonMonetary,
+                    other: labourOther
+                };
+
+                let pgAdminCostDetails = {
+                    phone: worksheet[col + '15'].v,
+                    mobile: worksheet[col + '16'].v,
+                    post: worksheet[col + '17'].v,
+                    books: worksheet[col + '18'].v,
+                    other: worksheet[col + '19'].v
+                };
+
+                let pgVariableCostDetails = {
+                    trip: worksheet[col + '22'].v,
+                    water_eletric: worksheet[col + '23'].v,
+                    repair: worksheet[col + '24'].v,
+                    consumable: worksheet[col + '25'].v,
+                    rental: worksheet[col + '26'].v,
+                    meeting: worksheet[col + '27'].v,
+                    advertisement: worksheet[col + '28'].v,
+                    greening: worksheet[col + '29'].v,
+                    hospitality: worksheet[col + '30'].v,
+                    decoration: worksheet[col + '31'].v,
+                    sales_service: worksheet[col + '32'].v,
+                    consultant: worksheet[col + '33'].v,
+                    services: worksheet[col + '34'].v,
+                    sampling: worksheet[col + '35'].v,
+                    inspection: worksheet[col + '36'].v,
+                    tax: worksheet[col + '37'].v,
+                    cleaning: worksheet[col + '38'].v,
+                    vehicle: worksheet[col + '39'].v,
+                    other: worksheet[col + '40'].v
+                };
+
+                let result = await costService.addCost(pgCost);
+                let costId = result[0].insertId;
+                // affectedRows
+
+                pgLabourCostDetails.cost_id = costId;
+                pgAdminCostDetails.cost_id = costId;
+                pgVariableCostDetails.cost_id = costId;
+
+                await costService.addLabourCostDetails(pgLabourCostDetails);
+                await costService.addAdminCostDetails(pgAdminCostDetails);
+                await costService.addVariableCostDetails(pgVariableCostDetails);
+            });
+        });
+
+
+
 
         ctx.body = 'ok';
     },
