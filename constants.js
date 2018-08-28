c8f20e5bbd2719897c716326dfccb163fafbba39
gxiewenyan <gxiewenyan@gmail.com>
2018-08-28 11:18:18 +0800

    增加jsonwebtoken secret

--- constants.js
+++ constants.js
@@ -5,5 +5,6 @@ module.exports = {
     PAGE_SIZE: 20,
     COMPARISON_CHART_LEGEND: ['人工成本', '办公费', '折旧费', '可变动成本', '低值易耗品摊销'],
     COMPARISON_CHART_LEGEND_EN: ['labour_cost', 'administrative_cost', 'depreciation_cost', 'variable_cost', 'consumable'],
-    BAR_COLORS: ['#c23531','#0032C6','#9C27B0','#36B022','#FFBE00']
+    BAR_COLORS: ['#c23531','#0032C6','#9C27B0','#36B022','#FFBE00'],
+    SECRET: 'SECRETsecret'
 };
